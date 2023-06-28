e1aa93a56794c671c360873dff195f8f7b7d18c0
TobyAI <toby.r@disc.ac.uk>
2023-06-28 14:49:46 +0100

    cors fix

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from controller import controller
 
 app = Flask("discoAPI")
@@ -7,7 +7,9 @@ app = Flask("discoAPI")
 @app.route('/items', methods=["GET"])
 def read_all_items():
   result = controller.find_all()
-  return result, 200
+  response = jsonify(result)
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  return response, 200
 
 
 if __name__ == "__main__":
